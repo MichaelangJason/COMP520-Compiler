4861a25ccbc7218a5252cbc79022ceb93a592081
Jiaju Nie <jiaju.nie@mail.mcgill.ca>
2024-01-26 10:46:04 -0500

    feat(Tokeniser): single & double char symbol

--- src/java/lexer/Tokeniser.java
+++ src/java/lexer/Tokeniser.java
@@ -39,11 +39,16 @@ public class Tokeniser extends CompilerPass {
         // skip white spaces between lexems
         if (Character.isWhitespace(c))
             return nextToken();
+        
+        Token temp; // initialized to null
 
-        // recognises the plus operator
-        if (c == '+')
-            return new Token(Token.Category.PLUS, line, column);
-
+        // no branch
+        if ((temp = noBranch(c, line, column)) != null) return temp;
+        
+        // 2 char branches
+        if ((temp = twoCharBranch(c, line, column)) != null) return temp;
+        
+        
         // ... to be completed
 
 
@@ -52,5 +57,83 @@ public class Tokeniser extends CompilerPass {
         return new Token(Token.Category.INVALID, line, column);
     }
 
+    private Token noBranch(char c, int line, int column) {
+        switch(c) {
+            case '+':
+                return new Token(Token.Category.PLUS, line, column);
+            case '-':
+                return new Token(Token.Category.MINUS, line, column);
+            case '*':
+                return new Token(Token.Category.ASTERIX, line, column);
+            case '/':
+                return new Token(Token.Category.DIV, line, column);
+            case '%':
+                return new Token(Token.Category.REM, line, column);
+            case '{':
+                return new Token(Token.Category.LBRA, line, column);
+            case '}':
+                return new Token(Token.Category.RBRA, line, column);
+            case '(':
+                return new Token(Token.Category.LPAR, line, column);
+            case ')':
+                return new Token(Token.Category.RPAR, line, column);
+            case '[':
+                return new Token(Token.Category.LSBR, line, column);
+            case ']':
+                return new Token(Token.Category.RSBR, line, column);
+            case ';':
+                return new Token(Token.Category.SC, line, column);
+            case ',':
+                return new Token(Token.Category.COMMA, line, column);
+            case '.':
+                return new Token(Token.Category.DOT, line, column);
+            default:
+                return null;
+        }
+    }
 
+    private Token twoCharBranch(char c, int line, int column) {
+        switch (c) {
+            case '|':
+                if (scanner.peek() == '|') {
+                    scanner.next(); // consume next | char
+                    return new Token(Token.Category.LOGOR, line, column);
+                }
+                error(c, line, column);
+                return new Token(Token.Category.INVALID, line, column);
+            case '&':
+                if (scanner.peek() == '&') {
+                    scanner.next(); // consume next & char
+                    return new Token(Token.Category.LOGAND, line, column);
+                }
+                return new Token(Token.Category.AND, line, column);
+            case '=':
+                if (scanner.peek() == '=') {
+                    scanner.next(); // consume next = char
+                    return new Token(Token.Category.EQ, line, column);
+                }
+                return new Token(Token.Category.ASSIGN, line, column);
+            case '!':
+                if (scanner.peek() == '=') {
+                    scanner.next(); // consume next = char
+                    return new Token(Token.Category.NE, line, column);
+                }
+                error(c, line, column);
+                return new Token(Token.Category.INVALID, line, column);
+            case '<':
+                if (scanner.peek() == '=') {
+                    scanner.next(); // consume next = char
+                    return new Token(Token.Category.LE, line, column);
+                }
+                return new Token(Token.Category.LT, line, column);
+            case '>':
+                if (scanner.peek() == '=') {
+                    scanner.next(); // consume next = char
+                    return new Token(Token.Category.GE, line, column);
+                }
+                return new Token(Token.Category.GT, line, column);
+            default:
+                return null;
+        }
+    }
 }
