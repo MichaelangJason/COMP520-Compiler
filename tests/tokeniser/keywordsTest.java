22bcfb9c8bfe3951bba17078dda430ba93bc992a
Jiaju Nie <jiaju.nie@mail.mcgill.ca>
2024-01-28 23:45:29 -0500

    feat(TokeniserTest): Keywords and SingleChar basic test

--- tests/tokeniser/keywordsTest.java
+++ tests/tokeniser/keywordsTest.java
@@ -1,5 +1,112 @@
 package tests.tokeniser;
 
-public class keywordsTest {
-    
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import lexer.Scanner;
+import lexer.Tokeniser;
+import lexer.Token;
+
+public class KeywordsTest {
+    Tokeniser s;
+
+    @Test
+    public void testINT() {
+        String expected = "INT".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.INT, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testIF() {
+        String expected = "IF".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.IF, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testVOID() {
+        String expected = "VOID".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.VOID, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testCHAR() {
+        String expected = "CHAR".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.CHAR, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testCONTINUE() {
+        String expected = "CONTINUE".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.CONTINUE, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testELSE() {
+        String expected = "ELSE".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.ELSE, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testWHILE() {
+        String expected = "WHILE".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.WHILE, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testRETURN() {
+        String expected = "RETURN".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.RETURN, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testSTRUCT() {
+        String expected = "STRUCT".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.STRUCT, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testSIZEOF() {
+        String expected = "SIZEOF".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.SIZEOF, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testBREAK() {
+        String expected = "BREAK".toLowerCase();
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.BREAK, t.category);
+        assertEquals(expected, t.data);
+    }
 }
--- tests/tokeniser/singleCharTest.java
+++ tests/tokeniser/singleCharTest.java
@@ -1,5 +1,130 @@
 package tests.tokeniser;
 
-public class singleCharTest {
-    
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import lexer.Scanner;
+import lexer.Token;
+import lexer.Tokeniser;
+
+public class SingleCharTest {
+    Tokeniser s;
+
+    @Test
+    public void testPLUS() {
+        String expected = "+";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.PLUS, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testMINUS() {
+        String expected = "-";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.MINUS, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testASTERIX() {
+        String expected = "*";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.ASTERIX, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testREM() {
+        String expected = "%";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.REM, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testLBRA() {
+        String expected = "{";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.LBRA, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testRBRA() {
+        String expected = "}";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.RBRA, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testLPAR() {
+        String expected = "(";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.LPAR, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testRPAR() {
+        String expected = ")";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.RPAR, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testLSBR() {
+        String expected = "[";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.LSBR, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testRSBR() {
+        String expected = "]";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.RSBR, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testSC() {
+        String expected = ";";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.SC, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testCOMMA() {
+        String expected = ",";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.COMMA, t.category);
+        assertEquals(expected, t.data);
+    }
+
+    @Test
+    public void testDOT() {
+        String expected = ".";
+        s = new Tokeniser(new Scanner(expected));
+        Token t = s.nextToken();
+        assertEquals(Token.Category.DOT, t.category);
+        assertEquals(expected, t.data);
+    }
 }
