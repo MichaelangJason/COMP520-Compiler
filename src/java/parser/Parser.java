42263d695acabfb0376a9e3a32e0726e84c95489
Jiaju Nie <jiaju.nie@mail.mcgill.ca>
2024-02-01 16:08:07 -0500

    feat(Parser): complete implementation

--- src/java/parser/Parser.java
+++ src/java/parser/Parser.java
@@ -8,6 +8,7 @@ import util.CompilerPass;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.stream.Stream;
 
 
 /**
@@ -123,11 +124,28 @@ public class Parser  extends CompilerPass {
             if (token.category == Category.STRUCT &&
                     lookAhead(1).category == Category.IDENTIFIER &&
                     lookAhead(2).category == Category.LBRA) {
+                // structdecl
                 parseStructDecl();
-            }
-            else {
-                // to be completed ...
-                nextToken(); // this line should be modified/removed
+            } else {
+                // type IDENT
+                parseType();
+                expect(Category.IDENTIFIER);
+
+                // fundecl or funproto
+                if (token.category == Category.LPAR) {
+                    parseFunPrime();
+
+                    if (token.category == Category.SC) {
+                        // funproto
+                        expect(Category.SC);
+                    } else {
+                        // fundecl
+                        parseBlock();
+                    }
+                } else {
+                    // vardecl
+                    parseVardeclPrime();
+                }
             }
         }
         // to be completed ...
@@ -144,14 +162,315 @@ public class Parser  extends CompilerPass {
         }
     }
 
+    /**
+     * structdecl ::= structtype "{" (vardecl)+ "}" ";"
+     */
     private void parseStructDecl(){
-        expect(Category.STRUCT);
-        expect(Category.IDENTIFIER);
+        // structtype
+        parseStructtype();
+        // "{"
         expect(Category.LBRA);
-        // to be completed ...
+        // (vardecl)+
+        parseType();
+        expect(Category.IDENTIFIER);
+        parseVardeclPrime();
+
+        while (accept(Category.INT, Category.CHAR, Category.VOID, Category.STRUCT)) {
+            parseType();
+            expect(Category.IDENTIFIER);
+            parseVardeclPrime();
+        }
+
+        expect(Category.RBRA);
+        expect(Category.SC);
     }
 
+    /**
+     * vardecl ::= type IDENT ("[" INT_LITERAL "]")* ";"
+     * vardecl' ::= ("[" INT_LITERAL "]")* ";"
+     */
+    private void parseVardeclPrime() {
+        while (accept(Category.LSBR)
+            && lookAhead(1).category == Category.INT_LITERAL
+            && lookAhead(2).category == Category.RSBR
+        ) {
+            expect(Category.LSBR);
+            expect(Category.INT_LITERAL);
+            expect(Category.RSBR);
+        }
+        expect(Category.SC);
+    }
 
+    /**
+     * type       ::= ("int" | "char" | "void" | structtype) ("*")*
+     * structtype ::= "struct" IDENT
+     */
+    private void parseType() {
+        if (accept(Category.INT, Category.CHAR, Category.VOID)) {
+            expect(Category.INT, Category.CHAR, Category.VOID);
+        } else if (accept(Category.STRUCT)) {
+            parseStructtype();
+        }
+        while (accept(Category.ASTERIX)) {
+            expect(Category.ASTERIX);
+        }
+    }
 
-    // to be completed ...
+    private void parseStructtype() {
+        expect(Category.STRUCT);
+        expect(Category.IDENTIFIER);
+    }
+
+    /*
+     * params     ::= [ type IDENT ("[" INT_LITERAL "]")* ("," type IDENT ("[" INT_LITERAL "]")*)* ]
+     */
+    private void parseParams() {
+        if (!accept(Category.INT, Category.CHAR, Category.VOID, Category.STRUCT)) return;
+        parseType();
+        expect(Category.IDENTIFIER);
+        // ("[" INT_LITERAL "]")*
+        while(accept(Category.LSBR)) {
+            expect(Category.LSBR);
+            expect(Category.INT_LITERAL);
+            expect(Category.RSBR);
+        }
+        // (("," type IDENT ("[" INT_LITERAL "]")*)*)
+        while(accept(Category.COMMA)) {
+            expect(Category.COMMA);
+            parseType();
+            expect(Category.IDENTIFIER);
+            // ("[" INT_LITERAL "]")*
+            while(accept(Category.LBRA)) {
+                expect(Category.LBRA);
+                expect(Category.INT_LITERAL);
+                expect(Category.RBRA);
+            }
+        }
+    }
+
+    /**
+     * block ::= "{" (vardecl)* (stmt)* "}"
+     */
+    private void parseBlock() {
+        expect(Category.LBRA);
+        
+        
+        // vardecl *
+        while (accept(Category.INT, Category.CHAR, Category.VOID, Category.STRUCT)) {
+            parseType();
+            expect(Category.IDENTIFIER);
+            parseVardeclPrime();
+        }
+
+        // stmt *
+        // parseStmt();
+        while (accept(
+            Category.LBRA, 
+            Category.WHILE, 
+            Category.IF, 
+            Category.RETURN, 
+            Category.CONTINUE, 
+            Category.BREAK,
+            // below are legal start token of an exp
+            Category.LPAR,
+            Category.IDENTIFIER,
+            Category.INT_LITERAL,
+            Category.MINUS,
+            Category.PLUS,
+            Category.CHAR_LITERAL,
+            Category.STRING_LITERAL,
+            Category.ASTERIX,
+            Category.AND,
+            Category.SIZEOF)) {
+                parseStmt();
+        }
+
+        expect(Category.RBRA);
+    }
+
+    private void parseStmt() {
+        if (accept(Category.WHILE)) {
+            expect(Category.WHILE);
+            expect(Category.LPAR);
+            parseExp();
+            expect(Category.RPAR);
+            parseStmt();
+        } else if (accept(Category.IF)) {
+            expect(Category.IF);
+            expect(Category.LPAR);
+            parseExp();
+            expect(Category.RPAR);
+            parseStmt();
+            if (accept(Category.ELSE)) {
+                expect(Category.ELSE);
+                parseStmt();
+            }
+        } else if (accept(Category.RETURN)) {
+            expect(Category.RETURN);
+            if (accept(Category.SC)) {
+                expect(Category.SC);
+            } else {
+                parseExp();
+                expect(Category.SC);
+            }
+        } else if (accept(Category.CONTINUE, Category.BREAK)) {
+            expect(Category.CONTINUE, Category.BREAK);
+            expect(Category.SC);
+        } else if (accept(Category.LBRA)) {
+            parseBlock();
+        } else {
+            parseExp();
+            expect(Category.SC);
+        }
+    }
+
+    /*
+     * A valid exp can start with:
+     * "(" IDENT INT_LITERAL "-" "+" CHAR_LITERAL STRING_LITERAL "*" "&" "sizeof"
+     */
+    private void parseExp() {
+        if (accept(Category.INT_LITERAL, Category.CHAR_LITERAL, Category.STRING_LITERAL)) {
+            nextToken(); // consume it
+        } else if (accept(Category.MINUS, Category.PLUS)) {
+            expect(Category.MINUS, Category.PLUS);
+            parseExp();
+        } else if (accept(Category.LPAR)) {
+            parseExpLpar();
+        } else if (accept(Category.ASTERIX)) {
+            parseValueat();
+        } else if (accept(Category.AND)) {
+            parseAddressof();
+        } else if (accept(Category.SIZEOF)) {
+            parseSizeof();
+        } else if (accept(Category.IDENTIFIER)) {
+            parseExpIdent();
+        }
+
+        // Exp'
+        parseExpPrime();
+    }
+
+    private void parseExpPrime() {
+        if (accept(Category.ASSIGN)) {
+            // assignment
+            expect(Category.ASSIGN);
+            parseExp();
+            parseExpPrime();
+        } else if (accept(Category.LSBR)) {
+            parseArrayAccessPrime();
+            parseExpPrime();
+        } else if (accept(Category.DOT)) {
+            parseFieldAccessPrime();
+            parseExpPrime();
+        } else if (accept(
+            Category.GT, 
+            Category.LT, 
+            Category.GE, 
+            Category.LE, 
+            Category.NE, 
+            Category.EQ, 
+            Category.PLUS, 
+            Category.MINUS,
+            Category.DIV,
+            Category.ASTERIX,
+            Category.REM,
+            Category.LOGOR,
+            Category.LOGAND)) 
+        {
+            // Binary Operations
+            nextToken(); // consume it
+            parseExp();
+            parseExpPrime();
+        }
+    }
+
+    private void parseExpIdent() {
+        expect(Category.IDENTIFIER);
+
+        if (accept(Category.LPAR)) {
+            // funcall
+            parseFuncall();
+        }
+    }
+
+    private void parseFuncall() {
+        expect(Category.LPAR);
+        if (accept(
+            Category.LPAR,
+            Category.IDENTIFIER,
+            Category.INT_LITERAL,
+            Category.MINUS,
+            Category.PLUS,
+            Category.CHAR_LITERAL,
+            Category.STRING_LITERAL,
+            Category.ASTERIX,
+            Category.AND,
+            Category.SIZEOF
+        ))
+        {
+            parseExp();
+            while (accept(Category.COMMA)) {
+                expect(Category.COMMA);
+                parseExp();
+            }
+        }
+        expect(Category.RPAR);
+    }
+
+    private void parseExpLpar() {
+        Category[] types = new Category[]{Category.INT, Category.CHAR, Category.VOID, Category.STRUCT};
+        if (Stream.of(types).anyMatch(s -> s == lookAhead(1).category)) {
+            parseTypecast();
+        } else {
+            expect(Category.LPAR);
+            parseExp();
+            expect(Category.RPAR);
+        }
+    }
+    /*
+     * fun' ::= "(" params ")"
+     */
+    private void parseFunPrime() {
+        expect(Category.LPAR);
+        parseParams();
+        expect(Category.RPAR);
+    }
+
+    void parseSizeof() {
+        if (accept(Category.SIZEOF)) {
+            expect(Category.SIZEOF);
+            expect(Category.LPAR);
+            parseType();
+            expect(Category.RPAR);
+        }
+    }
+
+    private void parseValueat() {
+        expect(Category.ASTERIX);
+        parseExp();
+    }
+    
+    private void parseAddressof() {
+        expect(Category.AND);
+        parseExp();
+    }
+
+    private void parseTypecast() {
+        expect(Category.LPAR);
+        parseType();
+        expect(Category.RPAR);
+        parseExp();
+    }
+
+    private void parseArrayAccessPrime() {
+        expect(Category.LSBR);
+        parseExp();
+        expect(Category.RSBR);
+    }
+
+    private void parseFieldAccessPrime() {
+        expect(Category.DOT);
+        expect(Category.IDENTIFIER);
+    }
+
 }
---
+++ tests/custom.c
@@ -0,0 +1,11 @@
+#include "asdfa"
+struct test1 {
+    int asdf;
+    char rqe;
+    void ckd;
+    struct test1 cdads;
+};
+
+void func1(int asdf, void asdfa, char fdii, struct test1 afds);
+
+// void func2(int asdf, void asdfa, char fdii, struct test1 afds) {}
