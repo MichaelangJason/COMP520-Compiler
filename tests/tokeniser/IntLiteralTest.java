c00d7e6376d93a96653d4d1c24d7ac2263243e25
Jiaju Nie <jiaju.nie@mail.mcgill.ca>
2024-02-02 14:45:56 -0500

    test(Tokeniser): Int Literal

--- tests/tokeniser/IntLiteralTest.java
+++ tests/tokeniser/IntLiteralTest.java
@@ -1,5 +1,66 @@
 package tests.tokeniser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import lexer.Scanner;
+import lexer.Token;
+import lexer.Tokeniser;
+
 public class IntLiteralTest {
-    
+    Tokeniser s;
+
+    @Test
+    public void testIntStart() {
+        for (int i = 9; i >= 0; i--) {
+            StringBuilder acc = new StringBuilder();
+
+            for (int j = i; j >= 0; j--)
+                acc.append(Integer.toString(j));
+
+            String expected = acc.toString();
+            s = new Tokeniser(new Scanner(expected));
+            Token t = s.nextToken();
+            assertEquals(Token.Category.INT_LITERAL, t.category);
+            assertEquals(expected, t.data);
+        }
+    }
+
+    @Test
+    public void testIntMix() {
+        for (int i = 9; i >= 0; i--) {
+                StringBuilder acc = new StringBuilder();
+
+            for (int j = i; j >= 0; j--)
+                acc.append(Integer.toString(j));
+
+            String expected = acc.toString();
+            String input = expected + "identifier";
+            s = new Tokeniser(new Scanner(input));
+            Token t = s.nextToken();
+            assertEquals(Token.Category.INT_LITERAL, t.category);
+            assertEquals(expected, t.data);
+            t = s.nextToken();
+            assertEquals(Token.Category.IDENTIFIER, t.category);
+            assertEquals("identifier", t.data);
+        }
+    }
+
+    @Test
+    public void testIntMixInverse() {
+        for (int i = 9; i >= 0; i--) {
+                StringBuilder acc = new StringBuilder();
+
+            for (int j = i; j >= 0; j--)
+                acc.append(Integer.toString(j));
+
+            String expected = "identifier" + acc.toString();
+
+            s = new Tokeniser(new Scanner(expected));
+            Token t = s.nextToken();
+            assertEquals(Token.Category.IDENTIFIER, t.category);
+            assertEquals(expected, t.data);
+        }
+    }
 }
