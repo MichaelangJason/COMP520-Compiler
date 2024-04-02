58d35c3039ab5527e47fad786e815724d0cdcc3d
Jiaju Nie <jiaju.nie@mail.mcgill.ca>
2024-04-02 16:58:56 -0400

    hotpatch

--- tests/gen/custom.asm
+++ tests/gen/custom.asm
@@ -1,30 +1,15 @@
 .data
+label_0_a:
+.byte 'a'
+.align 3
 
 .data
 # Allocated labels for virtual registers
-label_33_v11:
+label_6_v0:
 .space 4
-label_22_v8:
+label_10_v2:
 .space 4
 label_5_v1:
-.space 4
-label_18_v4:
-.space 4
-label_26_v7:
-.space 4
-label_15_v6:
-.space 4
-label_36_v9:
-.space 4
-label_8_v3:
-.space 4
-label_6_v2:
-.space 4
-label_27_v5:
-.space 4
-label_9_v0:
-.space 4
-label_41_v10:
 .space 4
 
 .text
@@ -39,140 +24,38 @@ movz $fp,$sp,$zero
 addiu $sp,$sp,-4
 # Original instruction: sw $ra,0($sp)
 sw $ra,0($sp)
-# Original instruction: addiu v0,$fp,-32
-addiu $t5,$fp,-32
-la $t0,label_9_v0
+# Original instruction: addiu v0,$fp,-5
+addiu $t5,$fp,-5
+la $t0,label_6_v0
 sw $t5,0($t0)
-# Original instruction: li v1,4
-li $t5,4
+# Original instruction: la v1,label_0_a
+la $t5,label_0_a
 la $t0,label_5_v1
 sw $t5,0($t0)
-# Original instruction: li v2,4
-li $t5,4
-la $t0,label_6_v2
-sw $t5,0($t0)
-# Original instruction: add v3,v1,v2
+# Original instruction: sw v1,0(v0)
 la $t5,label_5_v1
 lw $t5,0($t5)
-la $t4,label_6_v2
-lw $t4,0($t4)
-add $t3,$t5,$t4
-la $t0,label_8_v3
-sw $t3,0($t0)
-# Original instruction: sw v3,0(v0)
-la $t5,label_8_v3
-lw $t5,0($t5)
-la $t4,label_9_v0
+la $t4,label_6_v0
 lw $t4,0($t4)
 sw $t5,0($t4)
-# Original instruction: addiu v4,$fp,-28
-addiu $t5,$fp,-28
-la $t0,label_18_v4
+# Original instruction: addiu v2,$fp,-5
+addiu $t5,$fp,-5
+la $t0,label_10_v2
 sw $t5,0($t0)
-# Original instruction: addi v4,v4,4
-la $t4,label_18_v4
+# Original instruction: lb v2,0(v2)
+la $t4,label_10_v2
 lw $t4,0($t4)
-addi $t4,$t4,4
-la $t0,label_18_v4
-sw $t4,0($t0)
-# Original instruction: li v5,4
-li $t5,4
-la $t0,label_27_v5
-sw $t5,0($t0)
-# Original instruction: li v6,2
-li $t5,2
-la $t0,label_15_v6
-sw $t5,0($t0)
-# Original instruction: mult v6,v5
-la $t5,label_15_v6
-lw $t5,0($t5)
-la $t4,label_27_v5
-lw $t4,0($t4)
-mult $t5,$t4
-# Original instruction: mflo v5
-mflo $t5
-la $t0,label_27_v5
-sw $t5,0($t0)
-# Original instruction: add v5,v4,v5
-la $t5,label_18_v4
-lw $t5,0($t5)
-la $t3,label_27_v5
-lw $t3,0($t3)
-add $t3,$t5,$t3
-la $t0,label_27_v5
-sw $t3,0($t0)
-# Original instruction: addiu v8,$fp,-32
-addiu $t5,$fp,-32
-la $t0,label_22_v8
-sw $t5,0($t0)
-# Original instruction: addi v7,v8,0
-la $t5,label_22_v8
-lw $t5,0($t5)
-addi $t4,$t5,0
-la $t0,label_26_v7
-sw $t4,0($t0)
-# Original instruction: lw v7,0(v7)
-la $t4,label_26_v7
-lw $t4,0($t4)
-lw $t4,0($t4)
-la $t0,label_26_v7
-sw $t4,0($t0)
-# Original instruction: sw v7,0(v5)
-la $t5,label_26_v7
-lw $t5,0($t5)
-la $t4,label_27_v5
-lw $t4,0($t4)
-sw $t5,0($t4)
-# Original instruction: addiu v9,$fp,-28
-addiu $t5,$fp,-28
-la $t0,label_36_v9
-sw $t5,0($t0)
-# Original instruction: addi v9,v9,4
-la $t4,label_36_v9
-lw $t4,0($t4)
-addi $t4,$t4,4
-la $t0,label_36_v9
-sw $t4,0($t0)
-# Original instruction: li v10,4
-li $t5,4
-la $t0,label_41_v10
-sw $t5,0($t0)
-# Original instruction: li v11,2
-li $t5,2
-la $t0,label_33_v11
-sw $t5,0($t0)
-# Original instruction: mult v11,v10
-la $t5,label_33_v11
-lw $t5,0($t5)
-la $t4,label_41_v10
-lw $t4,0($t4)
-mult $t5,$t4
-# Original instruction: mflo v10
-mflo $t5
-la $t0,label_41_v10
-sw $t5,0($t0)
-# Original instruction: add v10,v9,v10
-la $t5,label_36_v9
-lw $t5,0($t5)
-la $t3,label_41_v10
-lw $t3,0($t3)
-add $t3,$t5,$t3
-la $t0,label_41_v10
-sw $t3,0($t0)
-# Original instruction: lw v10,0(v10)
-la $t4,label_41_v10
-lw $t4,0($t4)
-lw $t4,0($t4)
-la $t0,label_41_v10
+lb $t4,0($t4)
+la $t0,label_10_v2
 sw $t4,0($t0)
 # Original instruction: addiu $sp,$sp,-4
 addiu $sp,$sp,-4
-# Original instruction: sw v10,0($sp)
-la $t5,label_41_v10
+# Original instruction: sw v2,0($sp)
+la $t5,label_10_v2
 lw $t5,0($t5)
 sw $t5,0($sp)
-# Original instruction: jal print_i
-jal print_i
+# Original instruction: jal print_c
+jal print_c
 # Original instruction: addiu $sp,$sp,4
 addiu $sp,$sp,4
 # Original instruction: lw $ra,0($sp)
@@ -192,7 +75,7 @@ syscall
 # Allocated labels for virtual registers
 
 .text
-print_i:
+print_c:
 # Original instruction: addiu $sp,$sp,-4
 addiu $sp,$sp,-4
 # Original instruction: sw $fp,0($sp)
@@ -205,8 +88,10 @@ addiu $sp,$sp,-4
 sw $ra,0($sp)
 # Original instruction: lw $a0,4($fp)
 lw $a0,4($fp)
-# Original instruction: li $v0,1
-li $v0,1
+# Original instruction: lw $a0,0($a0)
+lw $a0,0($a0)
+# Original instruction: li $v0,11
+li $v0,11
 # Original instruction: syscall
 syscall
 # Original instruction: lw $ra,0($sp)
--- tests/gen/custom.c
+++ tests/gen/custom.c
@@ -1,19 +1,9 @@
-// void print_i(int i) {}
-struct t {
-    int* h;
-    int c[5];
-};
-
-
 void main() {
-    struct t test;
-    int n;
-    n = 4 + 4;
-    test.c[2] = *&n;
-    // &*test.h;
-    // test.c[0] = test.h;
-    // int b;
-    // b = n * n;
-    // print_i(b * b);
-    print_i(test.c[2]);
+    // struct t test;
+    // int c[2][3];
+    // c[0][0] = 1;
+    // c[1][2] = 4;
+    char a;
+    a = 'a';
+    print_c(a);
 }
